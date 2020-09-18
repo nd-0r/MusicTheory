8d30a8b115999f878579f8b3a21a3ef332046200
Rick Taube <taube@illiacsoftware.com>
2020-09-18 10:27:21 -0500

    commit hw3

---
+++ hw3/tet.py
@@ -0,0 +1,41 @@
+#
+# tet.py : Twelve-tone Equal Temperment (TET) 
+# See tet.html for module documentation.
+
+import math
+
+def hertz_to_midi(hertz):
+    pass
+
+
+def midi_to_hertz(midi):
+    pass
+
+
+def midi_to_pc(midi):
+    pass
+
+
+def pitch_to_midi(pitch):
+    pass
+
+
+def midi_to_pitch(midi, accidental=None):
+    pass
+
+
+def hertz_to_pitch(hertz):
+    pass
+
+
+def pitch_to_hertz(pitch):
+    pass
+
+
+if __name__ == '__main__':
+    print("Testing...")
+    
+    # add whatever test code you want here!
+
+    print("Done!")
+
