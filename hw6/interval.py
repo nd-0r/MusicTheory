24e561c3ea8d9c4d585b0577a795ef26da7f3bf1
Andrew Orals <ao@andreworals.com>
2020-10-20 11:25:31 -0500

    did __init__, _init_from_list_, _str_, and _repr_

--- hw6/interval.py
+++ hw6/interval.py
@@ -1,22 +1,30 @@
 ###############################################################################
 
 from .pitch import Pitch
+import random
 
 class Interval:
+
+    # span 0-7, qual 0-12, xoct 0-10, sign -1 or 1
 
     def __init__(self, arg, other=None):
         if (isinstance(arg, str)):
             self._init_from_string(arg)
-        elif (isinstance(arg, list)):
-            #add more checks
-            self._init_from_list(arg)
+        elif (isinstance(arg, list) and all((isinstance(e, int) for e in arg)) and len(arg) == 4):
+            self._init_from_list(arg[0], arg[1], arg[2], arg[3])
         elif (isinstance(arg, Pitch) and isinstance(other, Pitch)):
             self._init_from_pitches(arg, other)
         raise TypeError("Invalid input")
 
     
     def _init_from_list(self, span, qual, xoct, sign):
-        pass
+        if ((0 <= span <= 7) and (0 <= qual <= 12) and (0 <= xoct <= 10) and (sign == 1 or sign == -1)):
+            self.span = span
+            self.qual = qual
+            self.xoct = xoct
+            self.sign = sign
+        else:
+            raise ValueError(f'Values out of range for Interval.  span: {span} qual: {qual} xoct: {xoct} sign: {sign}')
 
     
     def _init_from_string(self, name):
@@ -28,11 +36,11 @@ class Interval:
     
 
     def __str__(self):
-        return ''
+        return f'<Interval: {self.string()} {hex(id(self))}>'
 
 
     def __repr__(self):
-        return ''
+        return f'Interval("{self.string()}")'
 
 
     def __lt__(self, other):
@@ -64,7 +72,7 @@ class Interval:
 
 
     def string(self):
-        pass
+        return ''
 
 
     def full_name(self, *, sign=True):
@@ -193,7 +201,11 @@ class Interval:
         pass
 
 
-if __name__ == '__main__':
+def test():
     # Add your testing code here!
-    pass
+    print(Interval([7, 1, 0, 1]))
+    print(Interval([random.randint(0, 7), random.randint(0, 12), random.randint(0, 10), random.randint(-1, 1)]))
 
+
+if __name__ == '__main__':
+    test()
