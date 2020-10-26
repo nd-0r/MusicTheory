e5d8513d27b746ab333a96e76c1b38b4fa820384
Andrew Orals <ao@andreworals.com>
2020-10-25 21:47:21 -0500

    new shit, spaceship

--- hw6/interval.py
+++ hw6/interval.py
@@ -171,7 +171,7 @@ class Interval:
         else:
             qual = self._qual_scale_imperfect_reverse[self._qual_scale_imperfect[base_interval.qual] + qual_offset]
         # self._init_from_list(base_interval.span, qual, max(abs(pitch2.octave - pitch1.octave) - 1, 0), base_interval.sign)
-        self._init_from_list(base_interval.span, qual, abs(pitch2.keynum() - pitch1.keynum()) // 12, base_interval.sign)
+        self._init_from_list(base_interval.span, qual, max((abs(pitch2.keynum() - pitch1.keynum()) // 12) - 1, 0), base_interval.sign)
             
 
     def __str__(self):
