f62639fa15d96127ee5b7b50db8ac04aa9ec0cfb
Andrew Orals <ao@andreworals.com>
2020-11-02 19:33:49 -0600

    virtual env

--- hw7/barline.py
+++ hw7/barline.py
@@ -4,6 +4,8 @@
 # Do not add or modify import statements.
 from enum import Enum, auto
 
+barlines = ('DASHED', 'DOTTED', 'FINAL_DOUBLE', 'HEAVY', 'INTERIOR_DOUBLE', 'LEFT_REPEAT', 'MIDDLE_REPEAT', )
+
 class Barline (Enum):
     # Create enums here...
     pass
