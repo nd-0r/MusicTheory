cb878cfc544c7b1f13eea315c98a87468e462b16
Andrew Orals <ao@andreworals.com>
2020-11-03 23:15:05 -0600

    created new virtualenv with python3.7 and worked on some stuff

--- hw7/key.py
+++ hw7/key.py
@@ -2,13 +2,14 @@
 # See key.html for module documentation.
 
 # Do not add or modify import statements.
-from .pitch import Pitch
+from .pitch import Pitch as P
 from .mode import Mode
 from .interval import Interval
 
 
-
 class Key:
+
+    _diatonic_intervals = ['P1', 'M2', 'm3', 'P4', 'P5', 'M6', 'M7']
 
     def __init__(self, signum, mode):
         assert (isinstance(signum, int))
@@ -28,16 +29,22 @@ class Key:
 
     def string(self):
         if (self.signum < -1):
-            k = Pitch.fromKeynum(69 + ((7 * 7) % 12), 'b').string()[:-1]
+            k = P.fromKeynum(69 + ((self.signum * 7) % 12), 'b').string()[:-1]
         elif (self.signum > 5):
-            k = Pitch.fromKeynum(69 + ((7 * 7) % 12), 's').string()[:-1]
+            k = P.fromKeynum(69 + ((self.signum * 7) % 12), 's').string()[:-1]
         else:
-            k = Pitch.fromKeynum(69 + ((7 * 7) % 12)).string()[:-1]
+            k = P.fromKeynum(69 + ((self.signum * 7) % 12)).string()[:-1]
         m = Mode(self.mode).name.lower()
         return (k + ' ' + m)
 
     def tonic(self):
-        pass
+        if (self.signum < -1):
+            p = P.fromKeynum(69 + ((self.signum * 7) % 12), 'b').string()[:-1]
+        elif (self.signum > 5):
+            p = P.fromKeynum(69 + ((self.signum * 7) % 12), 's').string()[:-1]
+        else:
+            p = P.fromKeynum(69 + ((self.signum * 7) % 12)).string()[:-1]
+        return Interval(self._diatonic_intervals[self.mode], p)
 
     def scale(self):
         pass
