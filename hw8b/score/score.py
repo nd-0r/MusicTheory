b0ec250d9b5d8d11c17f4b411f0c30a421dfe306
Rick Taube <taube@illiacsoftware.com>
2020-11-09 19:21:11 -0600

    commit hw8b

---
+++ hw8b/score/score.py
@@ -0,0 +1,47 @@
+###############################################################################
+
+# Do not alter, add, or delete import statements
+from .ratio import Ratio
+from .part import Part
+
+
+class Score:
+
+    def __init__(self, metadata={}, parts=[]):
+        pass
+
+    def __str__(self):
+        return ""
+
+    def __repr__(self):
+        return ""
+
+    def __iter__(self):
+        pass
+
+    def get_metadata(self, key, default=None):
+        pass
+
+    def set_metadata(self, key, value):
+        pass
+
+    def add_part(self, part):
+        pass
+
+    def part_ids(self):
+        pass
+
+    def num_parts(self):
+        pass
+
+    def get_part(self, pid):
+        pass
+
+    def print_all_repr(self):
+        reprs = []
+        indent = '  '
+        # Add your code here, don't forget to return the reprs list!
+
+    def print(self):
+        print('\n'.join(self.print_all_repr()))
+
