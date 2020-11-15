13094f9f3927b6ef341001afa5e3d4f54c87e01e
Andrew Orals <nd-0r@andreworals.com>
2020-11-14 20:06:52 -0600

    implemented chord

--- hw8b/score/chord.py
+++ hw8b/score/chord.py
@@ -8,13 +8,29 @@ from .note import Note
 class Chord(Durational):
 
     def __init__(self, notes):
-        pass
+        last = notes[0]
+        for n in notes:
+            if ((not isinstance(n, Note)) or (n.dur != last)):
+                raise TypeError('Invalid list of notes')
+            last = n
+        self.notes = notes
+        # not sure... can a chord be assigned to multiple voices?
+        self.voice = []
+        Durational.__init__(notes[0].dur)
 
     def __str__(self):
-        return ""
+        return str(f'<{self.__class__}: {self.string()} {hex(id(self))}>')
 
     def __repr__(self):
-        return ""
+        return str(f'<{self.__class__}: {self.string()}>')
 
     def string(self):
-        pass
+        return str(f'{list_string(self.notes)} {self.dur.string()}')
+
+    @staticmethod
+    def list_string(l):
+        out = '('
+        for element in l[:-1]:
+            out += str(element) + ', '
+        out += l[-1] + ')'
+        return out
