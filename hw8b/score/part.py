e2f79282c5748d8d5d717b6f2af73ef04a1ee7b4
Andrew Orals <nd-0r@andreworals.com>
2020-11-14 18:17:48 -0600

    did part

--- hw8b/score/part.py
+++ hw8b/score/part.py
@@ -7,22 +7,28 @@ from .staff import Staff
 class Part:
 
     def __init__(self, partid, name=None, shortname=None):
-        pass
+        self.partid = partid
+        self.name = name
+        self.shortname = shortname
+        self.staffs = []
+        self.score = None
 
     def __str__(self):
-        return ""
+        return str(f'<{self.__class__}: P{self.partid} {hex(id(self))}>')
 
     def __repr__(self):
-        pass
+        return str(f'<{self.__class__}: P{self.partid}>')
 
     def __iter__(self):
-        pass
+        return iter(self.staffs)
 
     def add_staff(self, staff):
-        pass
+        if (not isinstance(staff, Staff)):
+            raise TypeError(f'Invalid type: {type(staff)}')
+        self.staffs.append(staff)
 
     def staff_ids(self):
-        pass
+        return len(staffs)
 
     def num_staffs(self):
-        pass
+        return [s.id for s in staffs]
