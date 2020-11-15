193ed81247413917125fea1917358438e78e3fc7
Andrew Orals <nd-0r@andreworals.com>
2020-11-14 19:33:04 -0600

    did note

--- hw8b/score/note.py
+++ hw8b/score/note.py
@@ -9,31 +9,36 @@ from .pitch import Pitch
 class Note (Durational):
 
     def __init__(self, pitch, dur, marks=[]):
-        pass
+        if (not isinstance(pitch, Pitch)):
+            raise TypeError(f'Invalid pitch type: {Type(pitch)}')
+        Durational.__init__(dur)
+        self.pitch = pitch
+        self.marks = marks
+        self.voice = None
 
     def __str__(self):
-        return ""
+        return str(f'<{self.__class__}: {self.string()} {hex(id(self))}>')
 
     def __repr__(self):
-        return ""
+        return str(f'<{self.__class__}: {self.string()}>')
 
     def __lt__(self, other):
-        pass
+        return self.pitch.__lt__(other)
 
     def __le__(self, other):
-        pass
+        return self.pitch.__le__(other)
 
     def __eq__(self, other):
-        pass
+        return self.pitch.__eq__(other)
 
     def __ne__(self, other):
-        pass
+        return self.pitch.__ne__(other)
 
     def __ge__(self, other):
-        pass
+        return self.pitch.__ge__(other)
 
     def __gt__(self, other):
-        pass
+        return self.pitch.__gt__(other)
 
     def string(self):
-        pass
+        return str(f'{self.pitch.string()} {self.dur.string()}')
