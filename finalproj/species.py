9b36e7b30eaee6427a9f8e679ad43e4d31c891a3
Andrew Orals <nd-0r@andreworals.com>
2020-12-07 12:14:07 -0600

    fixed initial pep8 formatting issues

--- finalproj/species.py
+++ finalproj/species.py
@@ -1,41 +1,42 @@
 ###############################################################################
 
-## You can import from score, theory, and any python system modules you want.
+# You can import from score, theory, and any python system modules you want.
 
 from .score import Note, Pitch, Rest, Interval, Ratio, Mode, import_score
 from .theory import Analysis, Rule, timepoints
 from copy import copy
 from math import inf
 
-## Settings for a species 1 analysis. Pass this to SpeciesAnalysis() if you
+# Settings for a species 1 analysis. Pass this to SpeciesAnalysis() if you
 # are analyzing a species 1 score. See also: SpeciesAnalysis.
 s1_settings = {
     # section 1: melodic tests
-    ## Maximum number of melodic unisons allowed.
+    # Maximum number of melodic unisons allowed.
     'MAX_UNI': 1,
-    ## Maximum number of melodic 4ths allowed.
+    # Maximum number of melodic 4ths allowed.
     'MAX_4TH': 2,
-    ## Maximum number of melodic 5ths allowed.
+    # Maximum number of melodic 5ths allowed.
     'MAX_5TH': 1,
-    ## Maximum number of melodic 6ths allowed.
+    # Maximum number of melodic 6ths allowed.
     'MAX_6TH': 0,
-    ## Maximum number of melodic 7ths allowed.
+    # Maximum number of melodic 7ths allowed.
     'MAX_7TH': 0,
-    ## Maximum number of melodic 8vas allowed.
+    # Maximum number of melodic 8vas allowed.
     'MAX_8VA': 0,
-    ## Maximum number of leaps larger than a 3rd.
+    # Maximum number of leaps larger than a 3rd.
     'MAX_LRG': 2,
-    ## Maximum number of consecutive melodic intervals moving in same direction.
+    # Maximum number of consecutive melodic intervals
+    # moving in same direction.
     'MAX_SAMEDIR': 3,
 
     # section 2: harmonic tests
-    ## Maximum number of parallel consecutive harmonic 3rds/6ths.
+    # Maximum number of parallel consecutive harmonic 3rds/6ths.
     'MAX_PARALLEL': 3,
 
     # section 3: leap tests
-    ## Maximum number of consecutive leaps of any type.
+    # Maximum number of consecutive leaps of any type.
     'MAX_CONSEC_LEAP': 2,
-    ## Smallest leap demanding recovery step in opposite direction.
+    # Smallest leap demanding recovery step in opposite direction.
     'STEP_THRESHOLD': 5,
 
     # section 4: pitch checks
@@ -47,7 +48,7 @@ s1_settings = {
     'CADENCE_PATTERNS': [[2, 1], [7, 1]]
 }
 
-## Settings for species 2 analysis. Pass this to SpeciesAnalysis() if you
+# Settings for species 2 analysis. Pass this to SpeciesAnalysis() if you
 # are analyzing a second species score. See also: SpeciesAnalysis.
 s2_settings = copy(s1_settings)
 s2_settings['START_ABOVE'] = [1, 3, 5]
@@ -55,7 +56,7 @@ s2_settings['MAX_4TH'] = inf  # no limit on melodic fourths
 s2_settings['MAX_5TH'] = inf  # no limit on melodic fifths
 s2_settings['MAX_UNI'] = 0    # no melodic unisons allowed
 
-## A list of all the possible result strings your analysis can generate.
+# A list of all the possible result strings your analysis can generate.
 # The {} marker in each string will always receive the 1-based integer index
 # of the left-side time point that contains the offending issue. For example,
 # if the first timepoint (e.g. self.timepoints[0]) contained an illegal
@@ -76,13 +77,13 @@ result_strings = [
     'At #{}: voice overlap',
     'At #{}: voice crossing',
     'At #{}: forbidden weak beat dissonance',   # vertical dissonance
-    'At #{}: forbidden strong beat dissonance', # vertical dissonance
-    'At #{}: too many consecutive parallel intervals',  # parallel vertical intervals
+    'At #{}: forbidden strong beat dissonance',  # vertical dissonance
+    'At #{}: too many consecutive parallel intervals',  # parallel vert ints
 
     # MELODIC RESULTS
     'At #{}: forbidden starting pitch',
-    'At #{}: forbidden rest', 
-    'At #{}: forbidden duration',   
+    'At #{}: forbidden rest',
+    'At #{}: forbidden duration',
     'At #{}: missing melodic cadence',
     'At #{}: forbidden non-diatonic pitch',
     'At #{}: dissonant melodic interval',
@@ -94,8 +95,8 @@ result_strings = [
     'At #{}: too many leaps of an octave',      # 'MAX_8VA' setting
     'At #{}: too many large leaps',             # 'MAX_LRG' setting
     'At #{}: too many consecutive leaps,'        # 'MAX_CONSEC_LEAP' setting
-    'At #{}: too many consecutive intervals in same direction', # 'MAX_SAMEDIR' setting
-    'At #{}: missing reverse by step recovery', # 'STEP_THRESHOLD' setting
+    'At #{}: too many consecutive intervals in same direction',
+    'At #{}: missing reverse by step recovery',  # 'STEP_THRESHOLD' setting
     'At #{}: forbidden compound melodic interval',
     ]
 
@@ -103,14 +104,16 @@ result_strings = [
 
 
 
-## A class that implements a species counterpoint analysis of a given score.
+# A class that implements a species counterpoint analysis of a given score.
 # A SpeciesAnalysis has at least 5 attributes, you will very likely add more:
 #
 # * self.score  The score being analyzed.
 # * self.species  The integer species number of the analysis, either 1 or 2.
-# * self.settings  A settings dict for the analysis, either s1_settings or s2_settings.
+# * self.settings  A settings dict for the analysis, either s1_settings or
+# s2_settings.
 # * self.rules  An ordered list of Rules that constitute your analysis.
-# * self.results  A list of strings (see below) that constitute your analysis findings.
+# * self.results  A list of strings (see below) that constitute your analysis
+# findings.
 #
 # You should call your analysis like this:
 #
@@ -118,48 +121,49 @@ result_strings = [
 #   analysis = SpeciesAnalysis(score, 1, s1_settings)
 #   analysis.submit_to_grading()
 class SpeciesAnalysis(Analysis):
-    ## Initializes a species analysis.
+    # Initializes a species analysis.
     # @param score A score containing a two-part species composition.
     # @param species A counterpoint species number, either 1 or 2.
     def __init__(self, score, species):
-        ## Call the superclass and give it the score.
+        # Call the superclass and give it the score.
         super().__init__(score)
         if species not in [1, 2]:
             raise ValueError(f"'{species}' is not a valid species number 1 or 2.")
-        ## The integer species number for the analysis.
+        # The integer species number for the analysis.
         self.species = species
-        ## A local copy of the analysis settings.
+        # A local copy of the analysis settings.
         self.settings = copy(s1_settings) if species == 1 else copy(s2_settings)
-        ## Add your rules to this list.
+        # Add your rules to this list.
         self.rules = []
-        ## A list of strings that represent the findings of your analysis.
+        # A list of strings that represent the findings of your analysis.
         self.results = []
 
-    ## Use this function to perform whatever setup actions your rules require.
+    # Use this function to perform whatever setup actions your rules require.
     def setup(self, args, kwargs):
         pass
-    
-    ## This function is given to you, it returns your analysis results
+    # This function is given to you, it returns your analysis results
     # for the autograder to check.  You can also use this function as
     # a top level call for testing. Just make sure that it always returns
     # self.results after the analysis has been performed.
+    
     def submit_to_grading(self):
         self.analyze()
-        ## When you return your results to the autograder make sure you convert
+        # When you return your results to the autograder make sure you convert
         # it to a Python set, like this:
         return set(self.results)
-    
+
 ###############################################################################
+
 
 samples1 = ['1-018-C_ajyanez2.musicxml', '1-019-A_ajyanez2.musicxml',
             '1-005-A_hanzhiy2.musicxml', '1-008-C_davidx2.musicxml',
             '1-030_C_chchang6.musicxml', '1-011-B_weikeng2.musicxml',
             '1-037-A_sz18.musicxml', '1-012-B_erf3.musicxml',
             '1-030-C_cjrosas2.musicxml']
-                 
+
 samples2 = ['2-034-A_zawang2.musicxml', '2-028-C_hanzhiy2.musicxml',
             '2-000-B_sz18.musicxml', '2-003-A_cjrosas2.musicxml',
             '2-021-B_erf3.musicxml', '2-003_A_chchang6.musicxml',
             '2-009-C_mamn2.musicxml', '2-010-B_mamn2.musicxml',
-           '2-034-C_zawang2.musicxml', '2-029-A_hanzhiy2.musicxml',
-           '2-009-B_mamn2.musicxml', '2-021-C_erf3.musicxml']
+            '2-034-C_zawang2.musicxml', '2-029-A_hanzhiy2.musicxml',
+            '2-009-B_mamn2.musicxml', '2-021-C_erf3.musicxml']
